38bf4def88f0164e990b1a5b2e94c1997d360099
George Herășteanu <83351292+GeBeHash@users.noreply.github.com>
2021-11-11 21:26:43 +0200

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,27 @@
+import pyautogui
+import time
+
+text_file = input( "Read from text file ( 1 ) or input ( 2 ) ? " )
+if text_file == "1":
+    script = open("input", "r")
+    count = 5
+    while count != 0:
+        print(count)
+        time.sleep(1)
+        count -= 1
+    print("Fire in the hole!")
+    for word in script:
+        pyautogui.typewrite(word + '\n')
+elif text_file == "2":
+    msg = input("Message = ")
+    n = input("How many? ")
+    print("t minus ")
+    count = 5
+    while (count != 0):
+        print(count)
+        time.sleep(1)
+        count -= 1
+    print("Fire in the hole!")
+    for i in range(0, int(n)):
+        pyautogui.typewrite(msg + '\n')
+
